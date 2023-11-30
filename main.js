d112c3b8b059c173bfd04d4d42841515aa9f6a1a
ncrown1190 <ncrown88@outlook.com>
2023-11-30 18:20:40 -0500

    images added

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-// import Swiper from "swiper";
-
-import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
-
 var swiper = new Swiper(".swiper", {
   grabCursor: true,
   speed: 500,
@@ -12,7 +8,5 @@ var swiper = new Swiper(".swiper", {
     sensitivity: 1,
   },
 });
-
-const swiper = document.querySelector(".swiper").swiper;
 
 swiper.enable();
